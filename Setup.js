b8399a20a8c78ad2add7134b598188b697532acb
TurkDirk <evrenbasaran@misstudent.com>
2017-05-05 09:39:45 -0400

    Create Setup.js

---
+++ Setup.js
@@ -0,0 +1,19 @@
+/* 
+Car renting software
+Amount of sports cars, sedans, economy cars...
+Amount of cars currently rented
+Stock of cars
+Projected income
+Max possible income
+My SQL Server
+*/ 
+
+function setup() { 
+  createCanvas(400, 400);
+	var carsRented
+	var   
+} 
+
+function draw() { 
+  background(220);
+}
